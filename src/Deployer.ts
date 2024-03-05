463eec1ca997c0a14330947640190524702a7816
Peter Andringa <peter.andringa@ft.com>
2024-03-05 19:17:49 +0000

    print ig.in.ft.com url for preview deployments

--- src/Deployer.ts
+++ src/Deployer.ts
@@ -152,9 +152,12 @@ export default class Deployer extends EventEmitter {
 
     const urls = path ? [path] : targets.map((t) => this.getPath("/", t));
 
-    const domain = publicRead
-      ? `http://${bucket}.s3-website-${awsRegion}.amazonaws.com`
-      : `https://${bucket}.s3.${awsRegion}.amazonaws.com`;
+    const domain =
+      bucket === "djd-ig-preview"
+        ? `https://ig.in.ft.com/`
+        : publicRead
+        ? `http://${bucket}.s3-website-${awsRegion}.amazonaws.com`
+        : `https://${bucket}.s3.${awsRegion}.amazonaws.com`;
 
     return urls.map((url) => `${domain}/${url}`);
   }
